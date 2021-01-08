fcacc3bb35522bdfe391f26b60df195c435269d2
G-boy75 <77130059+G-boy75@users.noreply.github.com>
2021-01-07 21:05:03 -0500

    Add files via upload

---
+++ models/errors.go
@@ -0,0 +1,64 @@
+package models
+
+import "strings"
+
+var (
+	// ErrNotFound is returned when a resource cannot be found // in the database.
+	ErrNotFound modelError = "models: resource not found"
+
+	// ErrInvalidPassword is returned when an invalid password // is used when attempting to authenticate a user.
+	ErrPasswordIncorrect modelError = "models: incorrect password provided"
+
+	// ErrEmailRequired is returned when an email address is
+	// not provided when creating a user
+	ErrEmailRequired modelError = "models: email address is required"
+	//ErrEmailInvalid is returned when an email address provided
+	// does not match any of our requirements
+	ErrEmailInvalid modelError = "models: email address is not valid"
+
+	// ErrEmailTaken is returned when an update or create is attempted
+	// with an email address that is already in use.
+	ErrEmailTaken modelError = "models: email address is already taken"
+
+	// ErrPasswordRequired is returned when a create is attempted
+	// wihout a user password provided
+	ErrPasswordRequired modelError = "models: password is required"
+
+	// ErrPasswordTooShort is returned when an update or create is
+	// attempted with a user password that is less than 8 characters
+	ErrPasswordTooShort modelError = "models: password must be 8 characters long"
+
+	ErrTitleRequired modelError = "models: title is required"
+
+	// ErrRememberTooShort is returned when a remember token is
+	// not at least 32 bytes
+	ErrRememberTooShort privateError = "models: remember token must be at least 32 bytes"
+
+	// ErrRememberRequired is returned when a create or update
+	// is attempted wihout a user remember token hash
+	ErrRememberRequired privateError = "models: remember token is required"
+
+	ErrUserIDRequired privateError = "models: user ID is required"
+	// ErrIDInvalid is returned when an invalid ID is provided
+	// to a method like Delete.
+	ErrIDInvalid privateError = "models: ID provided was invalid"
+)
+
+type modelError string
+
+func (e modelError) Error() string {
+	return string(e)
+}
+
+func (e modelError) Public() string {
+	s := strings.Replace(string(e), "models: ", "", 1)
+	split := strings.Split(s, " ")
+	split[0] = strings.Title(split[0])
+	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
+}
