094aa63e5a46fe82bcc2c6a563c3ddfa7e6407b4
G-boy75 <77130059+G-boy75@users.noreply.github.com>
2021-01-07 21:04:42 -0500

    Add files via upload

---
+++ controllers/users.go
@@ -0,0 +1,161 @@
+package controllers
+
+import (
+	"net/http"
+	"time"
+
+	"lenslocked.com/context"
+	"lenslocked.com/models"
+	"lenslocked.com/rand"
+	"lenslocked.com/views"
+)
+
+func NewUsers(us models.UserService) *Users {
+	return &Users{
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
+	}
+}
+
+type Users struct {
+	NewView   *views.View
+	LoginView *views.View
+	us        models.UserService
+}
+
+// New is used to render the form where a user can
+// create a new user account.
+//
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	u.NewView.Render(w, r, nil)
+}
+
+type SignupForm struct {
+	Name     string `schema:"name"`
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Create is used to process the signup form when a user
+// tries to create a new user account.
+//
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
+		vd.SetAlert(err)
+		u.NewView.Render(w, r, vd)
+		return
+	}
+	user := models.User{
+		Name:     form.Name,
+		Email:    form.Email,
+		Password: form.Password,
+	}
+	if err := u.us.Create(&user); err != nil {
+		vd.SetAlert(err)
+		u.NewView.Render(w, r, vd)
+		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err := u.signIn(w, &user)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	alert := views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Welcome to Lenslocked.com!",
+	}
+	views.RedirectAlert(w, r, "/galleries", http.StatusFound, alert)
+
+}
+
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Login is used to process the login form when a user
+// tries to log in as an existing user (via email & pw).
+//
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	vd := views.Data{}
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+
+		vd.SetAlert(err)
+		u.LoginView.Render(w, r, vd)
+		return
+	}
+
+	user, err := u.us.Authenticate(form.Email, form.Password)
+	if err != nil {
+		switch err {
+		case models.ErrNotFound:
+			vd.AlertError("Invalid email address")
+		default:
+			vd.SetAlert(err)
+		}
+		u.LoginView.Render(w, r, vd)
+		return
+	}
+
+	err = u.signIn(w, user)
+	if err != nil {
+		vd.SetAlert(err)
+		u.LoginView.Render(w, r, vd)
+		return
+	}
+
+	http.Redirect(w, r, "/galleries", http.StatusFound)
+}
+
+// Logout is used to delete a users session cookie (remember_token)
+// and then will update the user resource with a new remember token
+// POST/logout
+func (u *Users) Logout(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    "",
+		Expires:  time.Now(),
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+
+	user := context.User(r.Context())
+	token, _ := rand.RememberToken()
+	user.Remember = token
+	u.us.Update(user)
+	http.Redirect(w, r, "/", http.StatusFound)
+
+}
+
+// signIn is used to sign the give user in via cookies
+func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
+	if user.Remember == "" {
+		token, err := rand.RememberToken()
+		if err != nil {
+			return err
+		}
+		user.Remember = token
+		err = u.us.Update(user)
+		if err != nil {
+			return err
+		}
+	}
+
+	cookie := http.Cookie{
+		Name:     "remember_token",
+		Value:    user.Remember,
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &cookie)
+	return nil
+}
---
+++ exp/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"lenslocked.com/models"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "asapalaran"
+	password = "your-password"
+	dbname   = "asapalaran"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	//us.AutoMigrate()
+	us.DestructiveReset()
+
+	user := models.User{
+		Name:     "Jon Calhoun",
+		Email:    "jon@calhoun.io",
+		Password: "jon",
+		Remember: "abc123",
+	}
+
+	err = us.Create(&user)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", user)
+	user2, err := us.ByRemember("abc123")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", *user2)
+}
